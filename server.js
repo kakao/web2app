4557da87ae56bfc787b4ed08f587fe31f280ad47
duhyeongKim <dewey.kim@daumkakao.com>
2014-10-08 10:27:33 +0900

    modify port to environment variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs"),
-    port = process.argv[2] || 8124;
+    port = process.env.HTTP_PORT || 8124;
 
 var exists = fs.exists || path.exists;
 http.createServer(function(request, response) {
